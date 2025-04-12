6ac7560596173b0c10b841c87e96062c68cd1cb5
tomas <tomas.navarro200013@gmail.com>
2025-04-12 15:29:39 -0300

    finishing challenge

--- app/chat/event_handler.py
+++ app/chat/event_handler.py
@@ -1,16 +1,12 @@
 from typing import Any
 import chainlit as cl
 from openai import AsyncAssistantEventHandler
-from literalai.helper import utc_now
-from app.config import Settings
 
 
 class EventHandler(AsyncAssistantEventHandler):
     def __init__(self, assistant_name: str) -> None:
         super().__init__()
         self.current_message: cl.Message = None
-        self.current_step: cl.Step = None
-        self.current_tool_call = None
         self.assistant_name = assistant_name
 
     async def on_text_created(self, *_: Any) -> None:
